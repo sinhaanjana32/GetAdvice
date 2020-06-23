4c9f75ecc273f2822e2ed9a868b87a4e3e1566f9
Anjana <sinhaanjana32@gmail.com>
2020-06-23 11:55:09 +0300

    Updates

---
+++ precache-manifest.3fd63d4cf46942df0e76ffac43e12956.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbc896048bc8899da5e31cbbd395fbb0",
+    "url": "/GetAdvice/index.html"
+  },
+  {
+    "revision": "c3a2c437219fff57d20c",
+    "url": "/GetAdvice/static/css/main.e854385b.chunk.css"
+  },
+  {
+    "revision": "800efa51a0ce77ef57f5",
+    "url": "/GetAdvice/static/js/2.0e05622a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/GetAdvice/static/js/2.0e05622a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3a2c437219fff57d20c",
+    "url": "/GetAdvice/static/js/main.052c863d.chunk.js"
+  },
+  {
+    "revision": "8c6ced430bd3f31fa99e",
+    "url": "/GetAdvice/static/js/runtime-main.d0f3622d.js"
+  }
+]);
